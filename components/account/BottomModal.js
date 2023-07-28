c83473a80f67e8344ac2bf0a43211816ba7f57c8
Nathanael-R <nathanielwealth20@gmail.com>
2023-07-28 22:13:23 +0100

    Finished Account page

--- components/account/BottomModal.js
+++ components/account/BottomModal.js
@@ -1,12 +1,11 @@
 import { View, Text, Pressable } from "react-native";
 import React from "react";
-import { BottomSheet } from "react-native-btr";
 const BottomModal = ({toggle}) => {
   return (
-    <View style={{backgroundColor: 'white'}}>
-      <Text>Wallet transfer</Text>
-      <Text>How would you like to make a transfer?</Text>
-      <View>
+    <View style={{backgroundColor: 'white', gap: 5}}>
+      <Text style={{textAlign: 'center'}}>Wallet transfer</Text>
+      <Text style={{textAlign: 'center'}}>How would you like to make a transfer?</Text>
+      <View style={{gap: 5}}>
       <Text>NGN Wallet to USD Wallet</Text>
       <Text>USD Wallet to NGN Wallet</Text>
       <Pressable onPress={toggle}>Close</Pressable>
--- components/topscreen/AccountContainer.js
+++ components/topscreen/AccountContainer.js
@@ -1,15 +1,21 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import {FontAwesome} from '@expo/vector-icons'
+import { View, Text, Dimensions } from "react-native";
+import React from "react";
+import { FontAwesome } from "@expo/vector-icons";
 const AccountContainer = () => {
+  const width = Dimensions.get("window").width - 35;
   return (
-    <View style={{ flexDirection: 'row', alignItems: 'center'}}>
-      <Text>
-        Account
-      </Text>
-      <FontAwesome name='gear' size={20} color='white'/>
+    <View
+      style={{
+        flexDirection: "row",
+        alignItems: "center",
+        width: width,
+        justifyContent: "space-between",
+      }}
+    >
+      <Text style={{fontWeight: 'bold', color: 'white'}}>Account</Text>
+      <FontAwesome name="gear" size={20} color="white" />
     </View>
-  )
-}
+  );
+};
 
-export default AccountContainer
+export default AccountContainer;
